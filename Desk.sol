5597f8a397e95f86f43f7cf85d1e3a5489dd56de
leventatbjss <levent.aksu@bjss.com>
2017-08-07 00:17:41 +0100

    Create Desk.sol

---
+++ Desk.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.0;
+contract Desk {
+
+    struct Deal {
+        address backer;
+        address layer;
+        uint16 price;
+        uint amount;
+    }
+    
+    struct Offer {
+        uint id;
+        address offeror;
+        uint16 price;
+        uint amount;
+    }
+
+    address barman;
+    
+    mapping(uint16=>Offer) heads;
+    mapping(uint=>Offer) next;
+    uint offerId;
+
+    function Desk() {
+        barman = msg.sender;
+    }
+    
+    function back(uint16 price) {
+        var offer = Offer(offerId++,msg.sender,price, msg.value);
+
+    }
+}
